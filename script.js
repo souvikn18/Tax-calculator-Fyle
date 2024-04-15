f74d8695e433a7b7d81a7633b29c63c9862a1948
Souvik Nath <souvikn20@gmail.com>
2024-04-15 12:21:56 +0530

    error handling issue and manual modal

--- script.js
+++ script.js
@@ -4,55 +4,54 @@ const ageGroup = document.getElementById("age")
 const deductions = document.getElementById("deductions")
 const finalValue = document.getElementById("final-income")
 const submitButton = document.getElementById("submit")
-const modal = document.getElementById("modal1")
+const modal = document.querySelector(".modal-custom")
+const modalBody = document.querySelector(".modal-custom-body")
+const closeModal = document.getElementById("close-modal")
 const errorTooltip1 = document.getElementById("errorTooltip1")
+const errorTooltip11 = document.getElementById("errorTooltip11")
 const errorTooltip2 = document.getElementById("errorTooltip2")
 const errorTooltip3 = document.getElementById("errorTooltip3")
 const ageTooltip = document.getElementById("ageTooltip")
 
-const errorIcon = document.createElement('i')
-
-errorIcon.className = "fa fa-exclamation-circle float-end position-absolute error"
-errorIcon.ariaHidden = true
-
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
-const checkString = () => {
-    if (isNaN(annualIncome.value) ) {
-        errorTooltip1.appendChild(errorIcon)
-    }else {
-        errorTooltip1.contains(errorIcon) && errorTooltip1.removeChild(errorIcon)
+const checkAnnualIncome = () => {
+    if (!annualIncome.value) {
+        errorTooltip11.classList.remove("hidden");
+    } else {
+        errorTooltip11.classList.add("hidden");
     }
-
-    if (isNaN(extraIncome.value) ) {
-        errorTooltip2.appendChild(errorIcon)
-    }else {
-        errorTooltip2.contains(errorIcon) && errorTooltip2.removeChild(errorIcon)
-    }
-
-    if (isNaN(deductions.value) ) {
-        errorTooltip3.appendChild(errorIcon)
-    }else {
-        errorTooltip3.contains(errorIcon) && errorTooltip3.removeChild(errorIcon)
+    
+    if (isNaN(annualIncome.value)) {
+        errorTooltip1.classList.remove("hidden");
+    } else {
+        errorTooltip1.classList.add("hidden");
     }
 }
-annualIncome.addEventListener('keyup', checkString)
-extraIncome.addEventListener('keyup', checkString)
-deductions.addEventListener('keyup', checkString)
 
+const checkAge = () => {
+    if (!ageGroup.value) {
+        ageTooltip.classList.remove("hidden");
+    } else {
+        ageTooltip.classList.add("hidden");
+    }
+}
 
 const handleSubmit = (e) => {
     e.preventDefault()
 
-    const overallIncome = Number(annualIncome.value) + Number(extraIncome.value) - Number(deductions.value)
-
-    const ageError = document.createElement('i')
-    ageError.className = 'fa fa-exclamation-circle float-end position-absolute error2'
-    ageError.ariaHidden = true
-    if (!ageGroup.value) {
-        ageTooltip.appendChild(ageError)
+    if (!annualIncome.value) {
+        checkAnnualIncome();
+        return;
     }
+    
+    if (!ageGroup.value) {
+        checkAge();
+        return;
+    }
+
+    const overallIncome = Number(annualIncome.value) + Number(extraIncome.value) - Number(deductions.value)
 
     if (overallIncome <= 800000 ) {
         alert('No need to pay tax')
@@ -74,9 +73,9 @@ const handleSubmit = (e) => {
         finalValue.innerHTML = finalIncome.toFixed(2)
     }
 
-    if (ageGroup.value == 30 || ageGroup.value == 40 || ageGroup.value == 10) {
-        ageTooltip.remove()
-    }
+    modal.classList.remove("hidden")
+    modalBody.classList.add("modal-animation")
+    document.body.style.overflow = "hidden"
 
     annualIncome.value = ""
     extraIncome.value = ""
@@ -84,4 +83,30 @@ const handleSubmit = (e) => {
     deductions.value = ""
 }
 
-submitButton.addEventListener('click', handleSubmit)
+annualIncome.addEventListener("keyup", checkAnnualIncome)
+
+extraIncome.addEventListener("keyup", () => {
+    if (isNaN(extraIncome.value)) {
+        errorTooltip2.classList.remove("hidden")
+    } else {
+        errorTooltip2.classList.add("hidden")
+    }
+})
+
+deductions.addEventListener("keyup", () => {
+    if (isNaN(deductions.value)) {
+        errorTooltip3.classList.remove("hidden")
+    } else {
+        errorTooltip3.classList.add("hidden")
+    }
+})
+
+ageGroup.addEventListener("change", checkAge)
+
+submitButton.addEventListener("click", handleSubmit)
+
+closeModal.addEventListener("click", () => {
+    modal.classList.add("hidden")
+    modalBody.classList.remove("modal-animation")
+    document.body.style.overflow = "scroll"
+})
